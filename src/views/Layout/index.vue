b9a83f5db982c4e8980d7c93fdda267a98c2630b
ALTXXX <1078369700@qq.com>
2026-01-01 22:53:52 +0800

    引入图标字体

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,7 +1,12 @@
-<template>
-  <div>我是首页
-<!--  二级路由表  -->
-    <RouterView/>
-  </div>
-</template>
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
 
+<template>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
+</template>
