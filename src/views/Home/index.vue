3bb5b955c388693864330a43239a3a0ae0f6331f
ALTXXX <1078369700@qq.com>
2026-01-02 20:52:23 +0800

    home-新鲜好物业务模块

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeCategory from "@/views/Home/components/HomeCategory.vue";
 import HomeProduct from "@/views/Home/components/HomeProduct.vue";
 import HomeHot from "@/views/Home/components/HomeHot.vue";
 import HomeNew from "@/views/Home/components/HomeNew.vue";
+import HomePanel from "@/views/Home/components/HomePanel.vue";
 </script>
 
 <template>
@@ -14,6 +15,13 @@ import HomeNew from "@/views/Home/components/HomeNew.vue";
   <HomeNew/>
   <HomeHot/>
   <HomeProduct/>
+<!-- 测试面板 -->
+  <HomePanel title="新鲜好物" subTitle="新鲜好物 好多商品">
+    <div>我是新鲜好物的插槽</div>
+  </HomePanel>
+  <HomePanel title="人气推荐" subTitle="人气推荐 好多商品">
+    <div>我是人气推荐的插槽</div>
 
+  </HomePanel>>
 </template>
 
